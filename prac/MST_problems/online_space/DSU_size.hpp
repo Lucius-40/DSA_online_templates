c37cc1ad158fad55d4171e7c1ddab6ba6196a7c0
Lucius-Viper <tanjinulislam16@gmail.com>
2026-01-19 19:30:15 +0600

    mst workspace added

---
+++ prac/MST_problems/online_space/DSU_size.hpp
@@ -0,0 +1,69 @@
+#pragma once
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pii pair<int,int>
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define F first
+#define S second
+
+namespace DSU{
+    int largest_set =0;
+    int largest_size = 1 ;
+    int components = -1 ;
+
+    struct Edge {
+        int u;
+        int v;
+        int weight;
+    };
+
+    vi set, size;
+
+    void make_set(int v){
+        set[v]=v ;
+        size[v]= 1 ;
+    }
+    int find_set(int v) {
+        if (v == set[v])
+            return v;
+        return set[v] = find_set(set[v]);
+    }
+
+    void union_set(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            if (size[a] < size[b]){
+                set[a] = b;
+                size[b]+= size[a];
+                if(size[b] > largest_size){
+                    largest_size = size[b];
+                    largest_set = b ;
+                }
+                
+            }
+            else {
+                set[b]=a ;
+                size[a]+=size[b];
+                if(size[a] > largest_size){
+                    largest_size = size[a];
+                    largest_set = a ;
+                }
+            }
+            components-- ;
+        }
+    }
+
+    void init(int n){
+        set.resize(n);
+        size.resize(n);
+        components=n ;
+        for(int i= 0 ; i < n ; i++)
+            make_set(i);
+    }
+}
---
+++ prac/MST_problems/online_space/problem_with_kruskal.cpp
@@ -0,0 +1,42 @@
+#include "DSU.hpp" 
+
+using namespace DSU ;
+
+bool comp(Edge a , Edge b){
+    return a.weight < b.weight ;
+}
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int n , m ;
+    cin >> n >> m ;
+    init(n);
+    vector<Edge> edges(m) ;
+    for(int i = 0 ; i < m ; i++){
+        int a , b, c ;
+        cin >> edges[i].u >> edges[i].v >> edges[i].weight ;
+    }
+    int root ; cin >> root ;
+
+    sort(edges.begin(), edges.end(), comp);
+
+    vector<Edge> mst ;
+    ll cost = 0 ;
+    for(auto e : edges){
+        if(find_set(e.u)!= find_set(e.v)){
+            mst.pb(e);
+            cost += e.weight ;
+            union_set(e.u, e.v);
+        }
+    }
+
+    cout <<"Total weight " <<cost << endl ;
+    for(auto e : mst){
+        cout << e.u <<" "<<e.v<<endl ;
+    }
+    
+
+
+
+}
